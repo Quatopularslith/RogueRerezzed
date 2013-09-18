3928c180ba8df4e26ca5befc4981cdf3b84cbecc
Mnenmenth <iamrazim12@gmail.com>
2013-09-18 16:59:53 -0400

    Update MainMenu.java

--- Git Test/src/Core/MainMenu.java
+++ Git Test/src/Core/MainMenu.java
@@ -1,28 +1,32 @@
 package Core;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class MainMenu extends JFrame {
+public class MainMenu extends JFrame implements ActionListener {
   public MainMenu() {
   	
     boolean mainMenuVis = true;
     boolean optMenuVis = false;
     
     JPanel mainMenu = new JPanel();
-    JLabel title = new JLabel("Rogue Rerezzed");
-    JButton newGame = new JButton("New Game");
-    JButton loadGame = new JButton("Load Game");
-    JButton options = new JButton("Options");
+    JLabel title = new JLabel();
+    JButton newGame = new JButton();
+    JButton loadGame = new JButton();
+    JButton options = new JButton();
     mainMenu.setVisible(mainMenuVis);
     
     JPanel optionsMenu = new JPanel();
-    JTextField fwdKB = new JTextField("Forward KeyBind");
-    JTextField backKB = new JTextField("Backwards KeyBind");
-    JTextField rightKB = new JTextField("Right KeyBind");
-    JTextField leftKB = new JTextField("Left KeyBind");
-    JTextField spellKB = new JTextField("Spell KeyBind");
-    JTextField eatKB = new JTextField("Eating KeyBind");
+    JTextField fwdKB = new JTextField();
+    JTextField backKB = new JTextField();
+    JTextField rightKB = new JTextField();
+    JTextField leftKB = new JTextField();
+    JTextField spellKB = new JTextField();
+    JTextField eatKB = new JTextField();
     mainMenu.setVisible(optMenuVis);
+    
+    
     
   }
 }
