5576367f0aa3aba95067e55134bad56f96bf3d76
Creatorri <torridon@live.com>
2013-09-18 09:43:25 -0400

    Update RogueEntity.java

--- Git Test/src/Entity/RogueEntity.java
+++ Git Test/src/Entity/RogueEntity.java
@@ -6,9 +6,12 @@ import Core.Game;
  * @author Torri
  */
 public abstract class RogueEntity extends Game{
-    public int x,y,z,dir;
+    public int x,y,dir;
     @Override
     public abstract void tick();
     @Override
     public abstract void render();
+    public abstract void getX();
+    public abstract void getY();
+    public abstract void getDir();
 }
