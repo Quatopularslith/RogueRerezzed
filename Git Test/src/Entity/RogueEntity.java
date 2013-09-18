6400511ff24a7d2a591ddaabea54772455e817a5
Creatorri <torridon@live.com>
2013-09-18 16:48:06 -0400

    Build 0.01, MORE NETBEANS

--- Git Test/src/Entity/RogueEntity.java
+++ Git Test/src/Entity/RogueEntity.java
@@ -9,8 +9,6 @@ public abstract class RogueEntity extends Game{
     public int x,y,dir;
     @Override
     public abstract void tick();
-    @Override
-    public abstract void render();
     public abstract void getX();
     public abstract void getY();
     public abstract void getDir();
