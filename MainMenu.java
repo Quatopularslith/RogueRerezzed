c051f0e0d30e7dbf616012e2f14c1ab76bc1335f
Mnenmenth <iamrazim12@gmail.com>
2013-09-18 09:59:58 -0400

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -1,1 +1,16 @@
-import
+import javax.swing.*;
+import java.awt.*;
+
+public class MainMenu extends JFrame {
+  public MainMenu(){
+    super("Rogue Rerezzed");
+    this.setSize(500, 500);
+    
+    private JLabel titleLabel = new JLabel("Rogue Rerezzed");
+    private JButton newGame = new JButton("New Game");
+    private JButton loadGame = new JButton("Load Game");
+    private JButton options = new JButton("Options");
+    
+    
+  }
+}
