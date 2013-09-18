a23ad494315207c0427bb0920adb9261518a5b33
Mnenmenth <iamrazim12@gmail.com>
2013-09-18 18:00:08 -0400

    Update MainMenu.java

--- Git Test/src/Core/MainMenu.java
+++ Git Test/src/Core/MainMenu.java
@@ -9,7 +9,11 @@ public class MainMenu extends JFrame implements ActionListener {
     boolean optMenuVis = false;
  
   public MainMenu() {
-      
+      MainMenu.setSize(750,500);
+      MainMenu.add(mainMenu);
+      MainMenu.add(optionsMenu);
+      MainMenu.setLookAndFeel();
+      MainMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     //Initialize JPanels, Button, and Stuff
     
     JPanel mainMenu = new JPanel();
@@ -18,6 +22,11 @@ public class MainMenu extends JFrame implements ActionListener {
     JButton loadGame = new JButton();
     JButton options = new JButton();
     mainMenu.setVisible(mainMenuVis);
+    mainMenu.setSize(750,500);
+    mainMenu.
+    
+    mainMenu.add()
+    
     
     JPanel optionsMenu = new JPanel();
     JTextField fwdKB = new JTextField();
@@ -26,8 +35,8 @@ public class MainMenu extends JFrame implements ActionListener {
     JTextField leftKB = new JTextField();
     JTextField spellKB = new JTextField();
     JTextField eatKB = new JTextField();
-    mainMenu.setVisible(optMenuVis);
-    
+    optionsMenu.setVisible(optMenuVis);
+    optionsMenu.setSize(750,500);
     
   }
     
