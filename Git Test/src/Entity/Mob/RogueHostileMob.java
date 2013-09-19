3ad53ed5618adb07b1b46b0362a4699ca87720fe
Creatorri <torridon@live.com>
2013-09-19 16:41:24 -0400

    Build 0.01, MORE NETBEANS

--- Git Test/src/Entity/Mob/RogueHostileMob.java
+++ Git Test/src/Entity/Mob/RogueHostileMob.java
@@ -6,7 +6,5 @@ import Entity.RogueEntity;
  * @author Torri
  */
 public abstract class RogueHostileMob extends RogueEntity{
-    @Override
-    public abstract void turn();
     public abstract int attack();
 }
