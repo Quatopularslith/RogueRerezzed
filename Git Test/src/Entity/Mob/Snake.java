9afd42687176773fffa82220b6b8524658a4850b
Creatorri <torridon@live.com>
2013-09-19 16:54:44 -0400

    Build 0.01, MORE NETBEANS

--- Git Test/src/Entity/Mob/Snake.java
+++ Git Test/src/Entity/Mob/Snake.java
@@ -1,17 +1,20 @@
 package Entity.Mob;
+import java.util.Random;
 /**
  * @author Torri
  */
 public class Snake extends RogueHostileMob{
+    Random r = new Random();
     public Snake(int spawnX, int spawnY){
         health=10;
+        maxAtt=2;
         armour=2;
         x = spawnX;
         y = spawnY;
     }
     @Override
     public int attack() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return r.nextInt(maxAtt);
     }
     @Override
     public int getX() {
