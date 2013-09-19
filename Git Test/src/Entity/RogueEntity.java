025f7fe2a2d1b5f9de42aaff1d2482f444b57540
Creatorri <torridon@live.com>
2013-09-19 16:44:48 -0400

    Build 0.01, MORE NETBEANS

--- Git Test/src/Entity/RogueEntity.java
+++ Git Test/src/Entity/RogueEntity.java
@@ -7,13 +7,9 @@ import Core.Game;
  */
 public abstract class RogueEntity extends Game{
     public int x,y,health,dir,armour;
-    @Override
-    public abstract void tick();
-    @Override
-    public abstract void turn();
-    public abstract void getX();
-    public abstract void getY();
-    public abstract void getDir();
-    public abstract void getHealth();
+    public abstract int getX();
+    public abstract int getY();
+    public abstract int getDir();
+    public abstract int getHealth();
     public abstract void damage(int att);
 }
