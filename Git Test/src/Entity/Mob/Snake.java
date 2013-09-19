3c8ae28b469c9505783fb2adab34fb41f166352f
Creatorri <torridon@live.com>
2013-09-19 10:50:38 -0400

    Update Snake.java

--- Git Test/src/Entity/Mob/Snake.java
+++ Git Test/src/Entity/Mob/Snake.java
@@ -4,10 +4,6 @@ package Entity.Mob;
  */
 public class Snake extends RogueHostileMob{
     @Override
-    public void tick() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    @Override
     public int attack() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
