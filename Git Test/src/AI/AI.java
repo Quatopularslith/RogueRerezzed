d9c545ca2fa408bc5973d6dbcf39d2ed32cb761b
Creatorri <torridon@live.com>
2013-09-19 17:08:51 -0400

    MOAR CODE 0.02

--- Git Test/src/AI/AI.java
+++ Git Test/src/AI/AI.java
@@ -6,7 +6,7 @@ import Entity.RogueEntity;
  * @author Torri
  */
 public class AI {
-    int direction;
+    int direction; // 0=down 1=downleft 2=left 3=upleft 4=up 5=upright 6=right 7=downright
     boolean doatt;
     public int pointTowards(RogueEntity e){
         return direction;
