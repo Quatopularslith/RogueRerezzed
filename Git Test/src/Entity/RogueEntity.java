3beaf9b29187cb4ae741ebf76da80cf0d727a776
Creatorri <torridon@live.com>
2013-09-20 10:31:00 -0400

    Update RogueEntity.java

--- Git Test/src/Entity/RogueEntity.java
+++ Git Test/src/Entity/RogueEntity.java
@@ -12,4 +12,7 @@ public abstract class RogueEntity extends Game{
     public abstract int getDir();
     public abstract int getHealth();
     public abstract void damage(int att);
+    public int distTo(RogueEntity e){
+        return (int) Math.sqrt((e.x-x)^2+(e.y-y)^2);
+    }
 }
