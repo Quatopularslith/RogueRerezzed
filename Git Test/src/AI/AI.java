e56e6d718673ccd0e864bb3859f34874832bf06a
Creatorri <torridon@live.com>
2013-09-20 10:30:04 -0400

    Update AI.java

--- Git Test/src/AI/AI.java
+++ Git Test/src/AI/AI.java
@@ -6,12 +6,40 @@ import Entity.RogueEntity;
  * @author Torri
  */
 public class AI {
-    int direction; // 0=down 1=downleft 2=left 3=upleft 4=up 5=upright 6=right 7=downright
+    double pdir,x,y,ex,ey;
+    int direction,round;
     boolean doatt;
+    RogueEntity here;
+    public AI(RogueEntity e){
+        here=e;
+        x=e.x;
+        y=e.y;
+    }
     public int pointTowards(RogueEntity e){
+        x=here.x;
+        y=here.y;
+        ex = e.x;
+        ey = e.y;
+        pdir = Math.atan((ey-y)/(ex-x));
+        pdir = Math.toDegrees(pdir);
+        if((ex-x)>=0 && (ey-y)>0){//Q1
+        }else if((ex-x)<=0 && (ey-y)>=0){//Q2
+            pdir += 270;
+        }else if((ex-x)<=0 && (ey-y)<=0){//Q3
+            pdir += 180;
+        }else if((ex-x)>=0 && (ey-y)<=0){//Q4
+            pdir += 90;
+        }
+        round = (int) pdir%90;
+        direction = 90*Math.round(round);
         return direction;
     }
-    public boolean doAttack(boolean ranged){
+    public boolean doAttack(boolean ranged, RogueEntity e){
+        if(ranged==false){
+            e.distTo(here);
+        }else{
+            
+        }
         return doatt;
     }
 }
