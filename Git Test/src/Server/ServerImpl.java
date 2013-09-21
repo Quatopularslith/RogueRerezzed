09090287278b1ba47aabf3a98e024b127cbbd93c
Creatorri <torridon@live.com>
2013-09-20 21:26:16 -0400

    0.022 The AI update; who knew it was that easy?

---
+++ Git Test/src/Server/ServerImpl.java
@@ -0,0 +1,12 @@
+package Server;
+import javax.jws.*;
+/**
+ * @author Torri
+ */
+@WebService(endpointInterface="Server.Server")
+public class ServerImpl implements Server{
+    @Override
+    public void getLevel() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
