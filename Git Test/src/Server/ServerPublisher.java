dea03b16aba698df5360766f4f4514646a5a13ed
Creatorri <torridon@live.com>
2013-09-20 21:31:19 -0400

    0.022 The AI update; who knew it was that easy?

--- Git Test/src/Server/ServerPublisher.java
+++ Git Test/src/Server/ServerPublisher.java
@@ -7,6 +7,6 @@ public class ServerPublisher {
     static String port = "5335";//TODO: add filereader to get the ip/intput
     public static void go(){
         ServerImpl si = new ServerImpl();
-        Endpoint.publish("http://127.0.0.1:"+port+"/clue", si);
+        Endpoint.publish("http://127.0.0.1:"+port+"/RogueRerezzed", si);
     }
 }
