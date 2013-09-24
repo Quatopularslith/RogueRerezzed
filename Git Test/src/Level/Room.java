f05443eeea82fcceef3648df4fa025d54c888727
Creatorri <torridon@live.com>
2013-09-24 09:41:52 -0400

    Update Room.java

--- Git Test/src/Level/Room.java
+++ Git Test/src/Level/Room.java
@@ -5,21 +5,11 @@ import Core.Game;
 /**
  * @author Torri
  */
-public class Room extends Game{
+public class Room{
     private int[] size;
     private int[] pos;
     private int[][] entrancepos;
-    public Room(int[] posin,int[] sizein,int[][] entrancepos){
+    public Room(int[] posin,int[] sizein,int[][] entrancepos, int maxlvl){
         
-    }
-
-    @Override
-    public void tick() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void turn() {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
