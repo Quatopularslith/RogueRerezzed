798179c487fd00360ba244344d5b16a259ddc45a
Creatorri <torridon@live.com>
2013-09-24 10:24:03 -0400

    Update Level.java

--- Git Test/src/Level/Level.java
+++ Git Test/src/Level/Level.java
@@ -12,11 +12,20 @@ public class Level {
     private RogueEntity[] re = new RogueEntity[maxEntities];
     private Player[] p = new Player[maxPlayers];
     private Player hp;
+    private int currentity = 0;
     public RogueEntity[] getEntities(){
         return re;
     }
     public Player[] getPlayers(){
         return p;
+    }
+    public void addEntity(RogueEntity e){
+        re[currentity]=e;
+    }
+    public void addEntities(RogueEntity[] e){
+        for(int i=0;i<e.length;i++){
+            addEntity(e[i]);
+        }
     }
     public Player getHostilePlayer(){
         for(int i=0;i<p.length;i++){
