73c40d36d03f988dcd39f9abaee9e47f0aaeb6e4
Creatorri <torridon@live.com>
2013-09-24 10:46:03 -0400

    Update Spawner.java

--- Git Test/src/Level/Spawner.java
+++ Git Test/src/Level/Spawner.java
@@ -1,7 +1,12 @@
 package Level;
 
 public class Spawner{
-    public Spawner(int x,int y,int am, int min, int max){
-       
+    Random r = new Random();
+    int lvl=0;
+    public Spawner(int x,int y,int am, int min, int max, Level l){
+        for(int i=0;i<am;i++){
+            lvl=min+(r.nextGaussian()*(max-min));
+            l.addEntity();
+        }
     }
 }
