3405ebb3707261d9f440567ba270719b9331a38c
Creatorri <torridon@live.com>
2013-09-25 19:38:39 -0400

    0.024.53 Just moving on. keyboard bugs can wait

--- Git Test/src/Level/Room.java
+++ Git Test/src/Level/Room.java
@@ -1,13 +1,19 @@
 package Level;
 
+import java.util.Random;
+
 /**
  * @author Torri
  */
 public class Room{
+    private Random r = new Random();
     private int[] size;
     private int[] pos;
-    private int[][] entrancepos;
+    private int[][] entpos;
     public Room(int[] posin,int[] sizein,int[][] entrancepos, int minlvl, int maxlvl){
+        size=sizein;
+        pos=posin;
+        entpos=entrancepos;
         
     }
 }
