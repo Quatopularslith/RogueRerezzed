37d5289d756b3ea6883ebf934b07d0186bc235dc
Creatorri <torridon@live.com>
2013-10-03 10:51:56 -0400

    Update Room.java

--- Git Test/src/Level/Room.java
+++ Git Test/src/Level/Room.java
@@ -7,10 +7,7 @@ import java.util.Random;
  */
 public class Room{
     private Random r = new Random();
-    private int[] size;
-    private int[] pos;
-    public Room(int[] posin,int[] sizein, int minlvl, int maxlvl){
-        size=sizein;
-        pos=posin;
+    public Room(int quuad,int[] sizein, int minlvl, int maxlvl){
+        
     }
 }
