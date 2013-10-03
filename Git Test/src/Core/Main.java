caee046613aa85ac5264814e06951b8d6100a89d
Creatorri <torridon@live.com>
2013-10-03 10:08:05 -0400

    Update Main.java

--- Git Test/src/Core/Main.java
+++ Git Test/src/Core/Main.java
@@ -4,12 +4,7 @@ package Core;
  * @author Torri
  */
 public class Main {
-    public static Game g;
     public static void main(String[] args){
-        //Loading l = new Loading(500);
-        g = new Game(750,500);
-    }
-    public static Game getGame(){
-        return g;
+        Loading l = new Loading(500);
     }
 }
