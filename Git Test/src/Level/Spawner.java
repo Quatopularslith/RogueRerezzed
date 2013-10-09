42c9b39eff20e9c56caed32ac785fb73e812195b
Creatorri <torridon@live.com>
2013-10-09 09:52:18 -0400

    Update Spawner.java

--- Git Test/src/Level/Spawner.java
+++ Git Test/src/Level/Spawner.java
@@ -6,6 +6,7 @@ import java.util.Random;
 public class Spawner{
     Random r = new Random();
     int lvl=0;
+    String s;
     public Spawner(int x,int y,int am, int min, int max, Level l,String type){
         if(type.equalsIgnoreCase("Snake")){
             for(int i=0;i<am;i++){
@@ -20,5 +21,15 @@ public class Spawner{
         }else{
             System.err.println("Ya. Wrong type man.");
         }
+    }
+    public Spawner(int x, int y, int am, int min, int max, Level l, int t){
+        if(t==0){
+            s="Snake";
+        }else if(t==1){
+            s="Bandit";
+        }else{
+            System.err.prinln("WRONG TYPE AKLHNLKFHADKLSHFKLNASDKLFHNSLAKNF");
+        }
+        this(x,y,am,min,max,l,s);
     }
 }
