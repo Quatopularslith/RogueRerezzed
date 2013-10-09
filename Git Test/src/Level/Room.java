3e47ca92c55892579713f53422081f2ca23a6666
Creatorri <torridon@live.com>
2013-10-09 09:52:16 -0400

    Update Room.java

--- Git Test/src/Level/Room.java
+++ Git Test/src/Level/Room.java
@@ -7,8 +7,16 @@ import java.util.Random;
  */
 public class Room{
     private Random r = new Random();
-    private int[] walls = new int[40];
-    public Room(int quuad,int[] sizein, int minlvl, Level l){
-        Spawner s = new Spawner();
+    private int[][] walls = new int[40][2];
+    private int[][] area = new int[100][2];
+    private int rs;
+    public Room(int quad,int[] size, int minlvl, Level l){
+        if(quad==0){
+            
+        }
+        rs = r.nextInt(99);
+        Spawner s = new Spawner(area[rs][0],area[rs][1],r.nextInt(20),minlvl,10,l,r.nextInt(2));
+        rs = r.nextInt(99);
+        Spawner s1 = new Spawner(area[rs][0],area[rs][1],r.nextInt(20),minlvl,10,l,r.nextInt(2));
     }
 }
