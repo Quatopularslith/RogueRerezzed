9b48b820aa1ecd806d95ed5a8a36835248073ac2
Creatorri <torridon@live.com>
2013-10-09 09:37:04 -0400

    Update Level.java

--- Git Test/src/Level/Level.java
+++ Git Test/src/Level/Level.java
@@ -2,20 +2,23 @@ package Level;
 
 import Entity.Player;
 import Entity.RogueEntity;
+import java.util.Random;
 
 /**
  * @author Torri
  */
 public class Level {
     // Level size is 500 by 500
+    private Random rand = new Random();
     private int maxEntities=100,maxPlayers=4, currentity = 0, index;
     private RogueEntity[] re = new RogueEntity[maxEntities];
     private Player[] p = new Player[maxPlayers];
+    private Room[] r = new Room[4];
     private Player hp;
     public int size = 500;
     public Level(int lvl){
         for(int i=0;i<4;i++){
-             
+             r[i] = new Room(i,{rand.nextInt(10),rand.nextInt(10)},rand.nextInt(10), this);
         }
     }
     public RogueEntity[] getEntities(){
