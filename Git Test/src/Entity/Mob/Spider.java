75b1eb8820c4958fc5d86166824117d511d06a18
Creatorri <torridon@live.com>
2013-10-10 09:58:52 -0400

    Update Spider.java

--- Git Test/src/Entity/Mob/Spider.java
+++ Git Test/src/Entity/Mob/Spider.java
@@ -1,4 +1,12 @@
 package Entity.Mob;
 
-public class Spider{
+public class Spider extends RogueHostileMob{
+    public Spider(int spawnX, int spawnY, int lvl){
+        health=10*(lvl/4);
+        maxAtt=2*(lvl/2);
+        armour=0;
+        x = spawnX;
+        y = spawnY;
+        dir = 0;
+    }
 }
