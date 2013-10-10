3b6a897e172eced47a5bf980b923e4b98c70b0e8
Creatorri <torridon@live.com>
2013-10-10 10:16:21 -0400

    Update Room.java

--- Git Test/src/Level/Room.java
+++ Git Test/src/Level/Room.java
@@ -10,13 +10,13 @@ public class Room{
     private int[][] walls = new int[40][2];
     private int[][] area = new int[100][2];
     private int rs;
-    public Room(int quad,int[] size, int minlvl, Level l){
-        if(quad==0){
+    public Room(int num,int[] size, int minlvl, Level l){
+        if(num==0){
             area[0][0] = r.nextInt(l.maxRoom-size[0]);
             area[0][1] = r.nextInt(l.maxRoom-size[1]);
             for(int i=0;i<size[0];i++){
                 for(int j=0;j<size[1];j++){
-                    
+                    area
                 }
             }
         }
