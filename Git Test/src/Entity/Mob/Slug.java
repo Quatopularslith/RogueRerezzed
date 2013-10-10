159d46e5adb631c383e64518d859f7ca3980f39f
Creatorri <torridon@live.com>
2013-10-10 09:58:17 -0400

    Update Slug.java

--- Git Test/src/Entity/Mob/Slug.java
+++ Git Test/src/Entity/Mob/Slug.java
@@ -1,1 +1,12 @@
 package Entity.Mob;
+
+public class Slug extends RogueHostileMob{
+    public Slug(int spawnX, int spawnY, int lvl){
+        health=5*(lvl/4);
+        maxAtt=(lvl/2);
+        armour=0;
+        x = spawnX;
+        y = spawnY;
+        dir = 0;
+    }
+}
