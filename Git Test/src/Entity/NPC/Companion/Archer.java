907b19c598a3ff5819f1d62d77dc6a3092d81ec9
Creatorri <>
2013-10-11 10:48:05 -0400

    jsb;vkch m.vnhbafjk.sbvj,

---
+++ Git Test/src/Entity/NPC/Companion/Archer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity.NPC.Companion;
+
+/**
+ *
+ * @author 1003749
+ */
+public class Archer {
+    
+}
