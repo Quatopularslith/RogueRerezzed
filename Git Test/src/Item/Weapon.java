2c10e70d0825ee23c03043d1aa2bfead9d95b260
Creatorri <>
2013-10-11 10:25:47 -0400

    jsb;vkch m.vnhbafjk.sbvj,

--- Git Test/src/Item/Weapon.java
+++ Git Test/src/Item/Weapon.java
@@ -7,9 +7,21 @@ import java.util.Random;
  */
 public class Weapon extends Item{
     private Random rand = new Random();
-    public int modifier;
+    public int mod;
     public String modName;
+    public int type;//0=sword
     public Weapon(){
-        
+        mod=rand.nextInt(10)-5;
+        if(mod<0){
+            modName = "Weak";
+        }else if(mod==0){
+            modName = null;
+        }else if(mod>0){
+            if(type==0){
+                modName="Sharp";
+            }else{
+                modName="Strong";
+            }
+        }
     }
 }
