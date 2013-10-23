fd237b2a4cb66c6324deb17aac395ddd7e3171d1
1003749 <1003749@HS-118-TEACHER.pasd.palisadessd.org>
2013-10-23 10:30:59 -0400

    HURRAH WE ARE DUN &^#**^!(*#^*(&$)*!&@#()$ WEB PROGRAMMING!

--- Git Test/src/Core/Cast.java
+++ Git Test/src/Core/Cast.java
@@ -5,6 +5,8 @@ package Core;
  */
 public class Cast {
     static int outi;
+    static boolean outb;
+    static int[] outia;
     static char[] outca;
     public static int stringtoInt(String s){
         outi=0;
@@ -48,8 +50,54 @@ public class Cast {
         return outi;
     }
     public static char[] inttoChararr(int num){
+        outb=false;
+        outia = new int[2];
+        outca = new char[2];
+        for(int i=1;i>0;i--){
+            outia[i] = num-10^i;
+            if(outb==true){
+                break;
+            }
+            System.out.println(outca[i-1]+" "+outia[i-1]+" is supposed to be:"+num);
+            switch(outia[i]){
+                case 0:
+                    outca[i]='0';
+                    break;
+                case 1:
+                    outca[i]='1';
+                    break;
+                case 2:
+                    outca[i]='2';
+                    break;
+                case 3:
+                    outca[i]='3';
+                    break;
+                case 4:
+                    outca[i]='4';
+                    break;
+                case 5:
+                    outca[i]='5';
+                    break;
+                case 6:
+                    outca[i]='6';
+                    break;
+                case 7:
+                    outca[i]='7';
+                    break;
+                case 8:
+                    outca[i]='8';
+                    break;
+                case 9:
+                    outca[i]='9';
+                    break;
+                default:
+                    outb=true;
+                    break;
+            }
+        }
         outca=new char[1];
         outca[0]=(char) num;
+        System.out.println("<br>");
         return outca;
     }
 }
