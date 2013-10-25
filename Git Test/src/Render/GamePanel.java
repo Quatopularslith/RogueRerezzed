d2d412628449fe8ced68a75d4e671e5e47f163e3
1003749 <1003749@HS-118-TEACHER.pasd.palisadessd.org>
2013-10-25 10:52:36 -0400

    LOL

---
+++ Git Test/src/Render/GamePanel.java
@@ -0,0 +1,16 @@
+package Render;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ * @author 1003749
+ */
+public class GamePanel extends JPanel{
+    public GamePanel(){
+        
+    }
+    public void paint(Graphics g){
+        
+    }
+}
