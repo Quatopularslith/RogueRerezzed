917a88c723130ee571cf1293862ab0b176600c55
1003749 <1003749@HS-118-TEACHER.pasd.palisadessd.org>
2013-11-01 10:53:17 -0400

    LOL

--- Git Test/src/Core/WorldSave.java
+++ Git Test/src/Core/WorldSave.java
@@ -1,13 +1,36 @@
 package Core;
 
+import Level.Level;
 import java.io.*;
+import java.util.Properties;
 
 public class WorldSave{
-    private File worldfile;
-    private FileInputStream inStream;
-    private FileOutputStream outStream;
-    public WorldSave(String dir){
+    private final File worldfile;
+    private final Level l;
+    private Properties config;
+    public WorldSave(String dir, Level l1){
         worldfile = new File(dir);
-        
+        l = l1;
+    }
+    public void addData(){
+        try {
+            worldfile.createNewFile();
+            try (FileInputStream inStream = new FileInputStream(worldfile)) {
+                config = new Properties();
+                config.load(inStream);
+                config.setProperty("Level",l.toString());
+            }
+        } catch (IOException ex) {
+            System.err.println("LAKJDNFDKLANLKBDKLABF ERROR ERROR ERROR ERROR; STAIRS!!!!!! "+ex.toString());
+        }
+    }
+    public void saveData(){
+        try {
+            try (FileOutputStream out = new FileOutputStream(worldfile)) {
+                config.store(out,"Properties settings");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace(System.err);
+        }
     }
 }
