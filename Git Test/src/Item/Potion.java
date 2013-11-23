59150a1f17b80210d7d0f6ebda226b6251b1c859
Creatorri <torridon@live.com>
2013-11-23 15:35:50 -0500

    Goodbye old code

--- Git Test/src/Item/Potion.java
+++
@@ -1,7 +0,0 @@
-package Item;
-/**
- * @author Torri
- */
-public class Potion {
-    
-}
--- Git Test/src/Level/Path.java
+++
@@ -1,7 +0,0 @@
-package Level;
-
-public class Path{
-  public Path(Room r1,Room r2,boolean secret,Level l){
-    
-  }
-}
