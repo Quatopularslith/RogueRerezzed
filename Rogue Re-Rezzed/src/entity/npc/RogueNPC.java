b28b35f31ae44b1335d77d534a45d97e2ee53af2
Creatorri <torridon@live.com>
2013-12-01 19:56:04 -0500

    v1.4.2: Cleaning stuff up.

---
+++ Rogue Re-Rezzed/src/entity/npc/RogueNPC.java
@@ -0,0 +1,15 @@
+
+package entity.npc;
+
+import dungeon.Level;
+import entity.RogueEntity;
+
+/**
+ * You read the name right! (look for this in the next update)
+ * @author Torri
+ */
+public class RogueNPC extends RogueEntity{
+    public RogueNPC(Level l1) {
+        super(l1);
+    }
+}
