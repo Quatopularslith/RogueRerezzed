858ffffc15bf38dab02c25582282e6bafb5cf661
1001503 <1001503@HS-118-05.pasd.palisadessd.org>
2013-12-18 10:24:29 -0500

    Started adding Debug Menu: Can add entities

---
+++ Rogue Re-Rezzed/src/util/RogueSave.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package util;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ *
+ * @author Mnenmenth
+ */
+public class RogueSave {
+
+    
+}
