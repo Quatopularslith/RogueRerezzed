a4ae13efbe3118354fa1370fa1f60c3ae01d4fa8
Creatorri <torridon@live.com>
2013-12-30 20:22:59 -0500

    ALPHA 1.3.0 High score Implementation, ready for consumer use (nearly)

--- Rogue High Score Server/src/hs/ServerPublisher.java
+++ Rogue High Score Server/src/hs/ServerPublisher.java
@@ -1,4 +1,7 @@
 package hs;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import javax.xml.ws.Endpoint;
 /**
  * @author Torri
@@ -6,9 +9,12 @@ import javax.xml.ws.Endpoint;
 public class ServerPublisher {
     public static void go(){
         try{
+            URL whatismyip = new URL("http://checkip.amazonaws.com");
+            BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+            String ip = in.readLine();
             ServerImpl si = new ServerImpl();
-            System.out.println("Started on 127.0.0.1:3000");
-            Endpoint.publish("http://127.0.0.1:3000/HighScore", si);
+            System.out.println(ip);
+            Endpoint.publish("http://127.0.0.1/HighScore", si);
         } catch(Exception e){
             System.err.println(e.toString());
         }
