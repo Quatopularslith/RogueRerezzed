a5d7eae2b0ccfe05bcd9979206bd068f214c4576
Creatorri <torridon@live.com>
2014-01-18 11:26:43 -0500

    1.7.4 BUG FIXES

--- Rogue Re-Rezzed/src/core/Rogue.java
+++ Rogue Re-Rezzed/src/core/Rogue.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
 import java.util.ArrayList;
 import ui.Menu;
 //http://semver.org/
