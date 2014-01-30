ae85908a3c25e879230c6ba894d32b7cf3bb0c54
Creatorri <torridon@live.com>
2014-01-30 16:26:32 -0500

    1.11 New Inventory Stuff

---
+++ Rogue Re-Rezzed/src/render/ItemSprite.java
@@ -0,0 +1,16 @@
+
+package render;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Torri
+ */
+public class ItemSprite{
+    public BufferedImage img;
+    public ItemSprite(ItemSheet i){
+    }
+    public ItemSprite(ItemSheet i,int dim){
+    }
+}
