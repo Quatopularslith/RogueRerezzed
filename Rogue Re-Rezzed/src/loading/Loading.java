68eac03dc5a716241763232dcd52c32944be4982
Creatorri <torridon@live.com>
2014-03-03 19:25:50 -0500

    Many much edit

--- Rogue Re-Rezzed/src/loading/Loading.java
+++ Rogue Re-Rezzed/src/loading/Loading.java
@@ -1,4 +1,5 @@
 package loading;
+
 import core.Rogue;
 import java.awt.Color;
 import java.awt.Font;
@@ -6,32 +7,22 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 import javax.swing.JPanel;
+
 public class Loading extends JPanel{
     double n = .875;
     int r, arrs = 201;
     double newTheta, theta = 2*Math.asin(1/(2*n));
-    double[] x=new double[arrs], y=new double[arrs];
     double cX, cY;
     public Loading(){
         int w = 750;
         this.setSize(w,w);
         this.setVisible(true);
-        r=w/4;
-        cX=(w/2)-(r/1.63);
-        cY=(w/2)-(r/1.63);
-        for(int i=0;i<(arrs-1);i++){
-            newTheta=i*theta;
-            x[i]=((0-r)*Math.sin(newTheta));
-            y[i]=(r*Math.cos(newTheta));
-            x[i]+=cX;
-            y[i]+=cY;
-        }
         LoadLoop.start();
         this.repaint();
     }
     public void update(){
         this.repaint();
-        if(Load.getTickNum()>=x.length-1){
+        if(Load.getTickNum()>=arrs){
             this.setVisible(false);
             if(Rogue.mm!=null){
                 LoadLoop.pause();
